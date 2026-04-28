c9e4d9cc3e9c8627e9541bdb69dc44fb802801a3
Codeur-Killer <elphenomenalo@gmail.com>
2026-04-28 13:00:48 +0200

    Correction du cors

--- server.js
+++ server.js
@@ -42,17 +42,15 @@ app.use(compression());
 // const allowedOrigins = (process.env.CORS_ORIGIN)
 //   .split(',').map(s => s.trim());
 
-app.use(cors({
- origin: process.env.CORS_ORIGIN,
- credentials: true,
- methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
- allowedHeaders: ['Content-Type','Authorization']
-}));
+// app.use(cors({
+//  origin: process.env.CORS_ORIGIN,
+//  credentials: true,
+//  methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
+//  allowedHeaders: ['Content-Type','Authorization']
+// }));
 
-app.use(cors({
- origin: process.env.CORS_ORIGIN,
- credentials:true
-}));
+app.use(cors());
+
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
